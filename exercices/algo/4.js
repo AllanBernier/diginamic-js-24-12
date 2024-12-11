96030655d0b321b0feeaa8832b432bcfbde671a4
Allan <allan.bernier1@gmail.com>
2024-12-11 14:44:45 +0100

    Algo exo 3

---
+++ exercices/algo/4.js
@@ -0,0 +1,31 @@
+// Créez un objet panier contenant une liste de produits représenté par (Nom, Quantité, Prix) . 
+// Écrivez une fonction ajouterProduitPanier qui :
+// Ajoute un produit à la liste.
+// Modifie la quantité si le produit existe déjà.
+// Retourne le prix total des produits dans le panier.
+
+const panier = []
+
+
+const ajouterProduitPanier = (name, price) => {
+
+  const index = panier.findIndex( (produit) => produit.name == name && produit.price == price )
+  
+  if (index !== -1){
+    panier[index].quantite ++ 
+  } else {
+    panier.push({name, price, quantite:1})
+  }
+
+  return panier.reduce( (acc, produit) => acc + produit.price * produit.quantite , 0)
+
+}
+
+
+console.log(ajouterProduitPanier("Pomme", 2))
+console.log(ajouterProduitPanier("Pomme", 2)) 
+console.log(ajouterProduitPanier("Banane", 3))
+console.log(ajouterProduitPanier("Orange", 4))
+console.log(ajouterProduitPanier("Pomme", 2))
+
+
